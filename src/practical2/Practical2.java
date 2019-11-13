06131a180bf81d086e17196daa280330234214e6
taruc <taruc@B25>
2019-11-13 10:50:15 +0800

    [

--- src/practical2/Practical2.java
+++ src/practical2/Practical2.java
@@ -17,6 +17,7 @@ public class Practical2 {
     public static void main(String[] args) {
         System.out.println("Hello");
         System.out.println("Hi");
+        cxpzkvpocxjb;lkvjm
     }
     
 }
